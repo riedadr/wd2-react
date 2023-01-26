be119b5e23a16c02f0bac6d325731a4f5a62ce77
riedadr@karsten <riedadr@gmail.com>
2023-01-26 18:02:35 +0100

    delete item ACHTUNG: Nach dem Löschen haben die Elemente keine einzigartige ID mehr

--- src/App.js
+++ src/App.js
@@ -3,74 +3,90 @@ import "./App.css";
 import Artikel from "./Artikel";
 
 export default class App extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         liste: [],
-         eingabe: { name: "", menge: "" },
-      };
-   }
+	constructor(props) {
+		super(props);
+		this.state = {
+			liste: [],
+			eingabe: { name: "", menge: "" },
+		};
+	}
 
-   addItem(e) {
-      e.preventDefault();
+	render() {
+		return (
+			<div>
+				<h1>Einkaufsliste</h1>
+				<form
+					className="d-flex gap-2"
+					onSubmit={(e) => {
+						e.preventDefault();
+						console.log(this.state.eingabe);
 
-      const newItem = {
-         ...this.state.eingabe, // name: this.state.eingabe.name, menge: this.state.eingabe.menge
-         id: this.state.liste.length + 1,
-      };
+						const id = this.state.liste.length + 1;
+						const newItem = {
+							...this.state.eingabe,
+							id: id,
+						};
 
-      this.setState({
-         liste: [...this.state.liste, newItem],
-      });
-   }
+						this.setState({
+							liste: [...this.state.liste, newItem],
+						});
+					}}
+				>
+					<input
+						type="text"
+						name="name"
+						placeholder="Name"
+						value={this.state.eingabe.name}
+						onChange={(e) => {
+							const wert = e.target.value;
+							this.setState({
+								eingabe: {
+									...this.state.eingabe,
+									name: wert,
+								},
+							});
+						}}
+					/>
+					<input
+						type="number"
+						name="menge"
+						placeholder="Menge"
+						value={this.state.eingabe.menge}
+						onChange={(e) => {
+							const wert = e.target.value;
+							this.setState({
+								eingabe: {
+									...this.state.eingabe,
+									menge: wert,
+								},
+							});
+						}}
+					/>
+					<button
+						className="bg-success border border-success text-white"
+						type="submit"
+						title="hinzufügen"
+					>
+						+
+					</button>
+				</form>
+				<ul>
+					{this.state.liste.map((element) => (
+						<Artikel
+							key={element.id}
+							name={element.name}
+							menge={element.menge}
+							deleteItem={() => {
+								const filteredList = this.state.liste.filter(
+									(item) => item.id !== element.id
+								);
 
-   setInput(e) {
-      const { name, value } = e.target;
-      this.setState({
-         eingabe: {
-            ...this.state.eingabe,
-            [name]: value,
-         },
-      });
-   }
-
-   render() {
-      return (
-         <div>
-            <h1>Einkaufsliste</h1>
-            <form className="d-flex gap-2" onSubmit={this.addItem.bind(this)}>
-               <input
-                  type="text"
-                  name="name"
-                  placeholder="Name"
-                  value={this.state.eingabe.name}
-                  onChange={this.setInput.bind(this)}
-               />
-               <input
-                  type="number"
-                  name="menge"
-                  placeholder="Menge"
-                  value={this.state.eingabe.menge}
-                  onChange={this.setInput.bind(this)}
-               />
-               <button
-                  className="bg-success border border-success text-white"
-                  type="submit"
-                  title="hinzufügen"
-               >
-                  +
-               </button>
-            </form>
-            <ul>
-               {this.state.liste.map((element) => (
-                  <Artikel
-                     key={element.id}
-                     name={element.name}
-                     menge={element.menge}
-                  />
-               ))}
-            </ul>
-         </div>
-      );
-   }
+								this.setState({ liste: filteredList });
+							}}
+						/>
+					))}
+				</ul>
+			</div>
+		);
+	}
 }
--- src/Artikel.jsx
+++ src/Artikel.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 export default class Artikel extends React.Component {
-   render() {
-      return <li>{this.props.name} ({this.props.menge})</li>;
-   }
+	render() {
+		return (
+			<li>
+				{this.props.name} ({this.props.menge})
+				<button onClick={this.props.deleteItem}>-</button>
+			</li>
+		);
+	}
 }
